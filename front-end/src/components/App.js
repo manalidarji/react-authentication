37f3c8d2837ac991ce02b473649a2d9b398cd965
Manali Darji <darjimanali93@gmail.com>
2022-03-09 21:32:21 -0500

    added route for sign up

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LogInPage from '../pages/LogInPage';
+import SignUpPage from '../pages/SignUpPage';
 
 const App = () => {
 	return(
 		<BrowserRouter>
 			<Routes>
 				<Route path='/login' element={<LogInPage/>} />
+				<Route path='/signup' element={<SignUpPage />} />
 			</Routes>
 		</BrowserRouter>
 	)
