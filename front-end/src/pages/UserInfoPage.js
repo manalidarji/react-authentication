fbe7e404822f7035739d4528093337fc926448d1
Manali Darji <darjimanali93@gmail.com>
2022-03-14 23:10:53 -0400

    user info page: added UI and JWT

--- front-end/src/pages/UserInfoPage.js
+++ front-end/src/pages/UserInfoPage.js
@@ -1,25 +1,60 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
+import useToken from '../auth/useToken';
+import useUser from '../auth/useUser';
+import axios from 'axios';
 
 const UserInfoPage = () => {
-	const [favoriteFood, setFavoriteFood] = useState('');
-	const [hairColor, setHairColor] = useState('');
-	const [bio, setBio] = useState('');
+	const [token, setToken] = useToken();
+	const user = useUser();
+	const {_id, email, info} = user;
 
+	const [favoriteFood, setFavoriteFood] = useState(info.favoriteFood || '');
+	const [hairColor, setHairColor] = useState(info.hairColor || '');
+	const [bio, setBio] = useState(info.bio || '');
+
+	const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+	// This useEffect hook automatically hides the success and error messages after 3 seconds when they're shown. Just a little user interface improvement.
+    useEffect(() => {
+        if (showSuccessMessage || showErrorMessage) {
+            setTimeout(() => {
+                setShowSuccessMessage(false);
+                setShowErrorMessage(false);
+            }, 3000);
+        }
+    }, [showSuccessMessage, showErrorMessage]);
+
+	// updating the user afte save btn was clicked
 	const saveChanges = async () => {
-			alert('Save Changes clicked');
+		try {
+			const response = await axios.put(`/api/users/${_id}`, 
+				{favoriteFood, hairColor, bio},
+				{headers: {Authorization: `Bearer ${token}`}}
+			);
+			setToken(response.data.token);
+			setShowSuccessMessage(true);
+		} catch (error) {
+			setShowErrorMessage(true);
+		}
 	}
 	
+	// resetting value to the initial values
 	const resetValues = () => {
-		alert('Reset Values clicked');
+		setFavoriteFood(info.favoriteFood);
+		setHairColor(info.hairColor);
+		setBio(info.bio);
 	}
 
 	const logOut = () => {
-			alert('Log out clicked');
+		alert('Log out clicked');
 	}
 
 	return (
 		<div className="content-container">
-			<h1>Info Page</h1>
+			<h1>Info Page for {email}</h1>
+			{showSuccessMessage && <div className="success">Successfully saved user data!</div>}
+            {showErrorMessage && <div className="fail">Uh oh... something went wrong and we couldn't save changes</div>}
 			<label>
 				Favorite Food:
 				<input
