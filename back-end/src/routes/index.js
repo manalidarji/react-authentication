27f5ba317f6d35c4bbc6a1af35ecf1d04f563802
Manali Darji <darjimanali93@gmail.com>
2022-03-17 16:34:18 -0400

    comment test send email route

--- back-end/src/routes/index.js
+++ back-end/src/routes/index.js
@@ -1,11 +1,11 @@
 import { loginRoute } from './loginRoute';
 import { signupRoute } from './signupRoute';
 import { updateUserInfoRoute } from './updateUserInfoRoute';
-import { testEmailRoute } from './testEmailRoute';
+// import { testEmailRoute } from './testEmailRoute';
 
 export const routes = [
     loginRoute,
     signupRoute,
     updateUserInfoRoute,
-    testEmailRoute
+    // testEmailRoute
 ];
